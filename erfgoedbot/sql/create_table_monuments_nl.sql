255ccd3eecaf8ee99e1e1836c2ab581b5be9b9b7
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-01-29 15:29:45 +0000

    Create the table for the Dutch monuments
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@5 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_nl.sql
@@ -0,0 +1,21 @@
+/* Create table statement for the Rijksmonumenten in the Netherlands table */
+connect p_erfgoed_p sql.toolserver.org;
+CREATE TABLE `monuments_nl` (
+  `objrijksnr` int(11) NOT NULL DEFAULT '0',
+  `woonplaats` varchar(255) NOT NULL DEFAULT '',
+  `adres` varchar(255) NOT NULL DEFAULT '',
+  `objectnaam` varchar(255) NOT NULL DEFAULT '',
+  `type_obj` ENUM('G', 'A'),
+  `oorspr_functie` varchar(128) NOT NULL DEFAULT '',
+  `bouwjaar`  varchar(255) NOT NULL DEFAULT '',
+  `architect` varchar(255) NOT NULL DEFAULT '',
+  `cbs_tekst` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`objrijksnr`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
