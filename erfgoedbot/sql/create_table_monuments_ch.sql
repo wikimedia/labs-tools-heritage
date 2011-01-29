de2af19f8737fc5081fc9749ee6c77bb49b87fa8
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-01-29 15:33:39 +0000

    Create table statement for Switzerland
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@6 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_ch.sql
@@ -0,0 +1,16 @@
+/* Create table statement for the monuments in Switzerland */
+connect p_erfgoed_p sql.toolserver.org;
+CREATE TABLE `monuments_ch` (
+  `kgs_nr` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `municipality` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`kgs_nr`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
