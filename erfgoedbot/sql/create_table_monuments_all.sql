8e215cc7b3fe4d3d235fd6653b6aad0e3094e281
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-01-30 22:39:56 +0000

    View does not work. Try making a table of it
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@22 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_all.sql
@@ -0,0 +1,17 @@
+/* Create view for all country tables */
+connect p_erfgoed_p sql.toolserver.org;
+CREATE TABLE `monuments_all` (
+  `country` varchar(10) NOT NULL DEFAULT '',
+  `id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `municipality` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`country`, `id`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
