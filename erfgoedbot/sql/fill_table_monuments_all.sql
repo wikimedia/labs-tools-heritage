cd778cda00d1eeeb2615e930304418810ef4fc21
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-02-01 21:54:52 +0000

    Sort by countrycode
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@28 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/sql/fill_table_monuments_all.sql
+++ erfgoedbot/sql/fill_table_monuments_all.sql
@@ -1,8 +1,21 @@
 /* Create view for all country tables */
 connect p_erfgoed_p sql.toolserver.org;
 TRUNCATE TABLE monuments_all;
-/* Switzerland */
 REPLACE INTO `monuments_all`
+/* Vlaanderen */
+(SELECT 'be-vlg' AS `country`,
+	`id` AS `id`,
+        `objectnaam` AS `name`,
+        `adres` AS `address`,
+        `gemeente` AS `municipality`,
+        `lat` AS `lat`,
+        `lon` AS `lon`,
+        `image` AS `image`,
+        `source` AS `source`,
+        `changed` AS `changed`
+        FROM `monuments_be-vlg`);
+UNION ALL
+/* Switzerland */
 (SELECT 'ch' AS `country`,
 	`kgs_nr` AS `id`, 
 	`name` AS `name`,
@@ -27,16 +40,3 @@ UNION ALL
 	`source` AS `source`,
 	`changed` AS `changed`
 	FROM `monuments_nl`)
-UNION ALL
-/* Vlaanderen */
-(SELECT 'be-vlg' AS `country`,
-	`id` AS `id`,
-        `objectnaam` AS `name`,
-        `adres` AS `address`,
-        `gemeente` AS `municipality`,
-        `lat` AS `lat`,
-        `lon` AS `lon`,
-        `image` AS `image`,
-        `source` AS `source`,
-        `changed` AS `changed`
-        FROM `monuments_be-vlg`);
