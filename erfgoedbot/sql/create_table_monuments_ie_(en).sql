e7166957b9d70b48ba3228ffbb0fc35fd81a25d5
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-02-19 11:13:42 +0000

    Ireland!
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@52 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_ie_(en).sql
@@ -0,0 +1,17 @@
+/* Create table statement for the monuments in Ireland */
+connect p_erfgoed_p sql.toolserver.org;
+CREATE TABLE `monuments_ie_(en)` (
+  `number` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `townland` varchar(255) NOT NULL DEFAULT '',
+  `county` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`number`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
