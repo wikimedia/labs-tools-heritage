3d4775373d721bf5bd77fee8c1b792d35deabc42
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-03-12 14:38:44 +0000

    Add field "nomcoor"
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@60 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/sql/create_table_monuments_ad_(ca).sql
+++ erfgoedbot/sql/create_table_monuments_ad_(ca).sql
@@ -1,5 +1,6 @@
 /* Create table statement for the monuments in Andorra in Catalan table */
 connect p_erfgoed_p sql.toolserver.org;
+DROP TABLE IF EXISTS `monuments_ad_(ca)`;
 CREATE TABLE `monuments_ad_(ca)` (
   `id` int(11) NOT NULL DEFAULT '0',
   `nom` varchar(255) NOT NULL DEFAULT '',
@@ -8,6 +9,7 @@ CREATE TABLE `monuments_ad_(ca)` (
   `lloc` varchar(255) NOT NULL DEFAULT '',
   `lat` double NOT NULL DEFAULT '0',
   `lon` double NOT NULL DEFAULT '0',
+  `nomcoor` varchar(255) NOT NULL DEFAULT '',
   `imatge` varchar(255) NOT NULL DEFAULT '',
   `source` varchar(255) NOT NULL DEFAULT '',
   `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
