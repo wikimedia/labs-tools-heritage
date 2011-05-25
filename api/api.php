a68733dc7daefe93e20c9f5c8101ccbcc45c8678
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2011-05-25 22:35:12 +0000

    One dirname() missing
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@120 3e45a915-b2e0-c146-c98e-857251466b57

--- api/api.php
+++ api/api.php
@@ -2,7 +2,7 @@
 
 /* Entry point for the monuments api */
 require dirname( __FILE__ ) . '/autoloader.php';
-require dirname( dirname( __FILE__ ) ) . '/database.inc';
+require dirname( dirname( dirname( __FILE__ ) ) ) . '/database.inc';
 
 Database::define(Monuments::$dbServer, Monuments::$dbTable, 
 	Monuments::$dbUser, $toolserver_password );
