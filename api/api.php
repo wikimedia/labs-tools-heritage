a4b514a12f3f307df5da157a840b3c9d236d9d0e
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2011-05-25 22:06:35 +0000

    Update the access credentials, using /home/project/e/r/f/erfgoed/database.inc
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@119 3e45a915-b2e0-c146-c98e-857251466b57

--- api/api.php
+++ api/api.php
@@ -2,9 +2,10 @@
 
 /* Entry point for the monuments api */
 require dirname( __FILE__ ) . '/autoloader.php';
+require dirname( dirname( __FILE__ ) ) . '/database.inc';
 
 Database::define(Monuments::$dbServer, Monuments::$dbTable, 
-	Monuments::$dbUser, file_get_contents( 'secretPassword' ) );
+	Monuments::$dbUser, $toolserver_password );
 
 $api = new ApiMonuments();
 $api->execute();
