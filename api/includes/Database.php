c9d978bce0dd8643d1cd7da242efa287844be5d4
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2011-05-25 22:38:12 +0000

    Using $this when not in object context
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@123 3e45a915-b2e0-c146-c98e-857251466b57

--- api/includes/Database.php
+++ api/includes/Database.php
@@ -44,7 +44,7 @@ class Database {
 	static function define($server, $database, $username, $password) {
 		self::$singleton = new Database();
 		self::$singleton->db = @mysql_connect( $server, $username, $password );
-		if ( !$this->db ) {
+		if ( !self::$singleton->db ) {
 			return false;
 		}
 		return mysql_select_db( $database, $this->db );
