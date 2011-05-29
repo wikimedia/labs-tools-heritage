f2e9964dcbb7a7b1d01216066b964e3d9af23956
kentaur <kentaur@3e45a915-b2e0-c146-c98e-857251466b57>
2011-05-29 12:45:32 +0000

    rewrite
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@160 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/add_coord_to_articles.py
+++ erfgoedbot/add_coord_to_articles.py
@@ -36,7 +36,9 @@ wikiData = {
     }
 }
 
+
 # "constants"
+
 # wikipedia article namespace
 WP_ARTICLE_NS = 0
 # wikipedia category namespace
@@ -45,6 +47,21 @@ WP_CATEGORY_NS = 14
 DEBUG = True
 
 
+# classes
+
+class Monument:
+#Constructor with default arguments
+   def __init__(self, id = None):
+     self.id = id
+     self.name = None
+     self.country = None
+     self.wikilang = None
+     self.article = None
+     self.lat = None
+     self.lon = None
+     self.source = None
+     
+     
 # functions
 
 def connectMonDatabase():
@@ -77,7 +94,7 @@ def processCountry(countrycode, lang, countryconfig, coordconfig, connMon, curso
     '''
     if (not coordconfig or not coordconfig.get('coordTemplate')):
         # No template found, just skip.
-        wikipedia.output(u'Language %s has no coordTemplate set!' % lang)
+        wikipedia.output(u'Language: %s has no coordTemplate set!' % lang)
         return False
 
     (connWiki, cursorWiki) = connectWikiDatabase(lang)
@@ -86,46 +103,43 @@ def processCountry(countrycode, lang, countryconfig, coordconfig, connMon, curso
 
     articleNames = []
     duplicateArticles = []
-    lat = {}
-    lon = {}
+    monumentsWithArticle = []
     
-    
-    for (mon_name, mon_lat, mon_lon) in withCoordinates:
+    for aMonument in withCoordinates:
         article_name = u''
-        result = re.match("\[\[(.+?)\|.+?\]\]", mon_name)
+        result = re.match("\[\[(.+?)\|.+?\]\]", aMonument.name)
         if (result and result.group(1)): 
             article_name = result.group(1)
-            lat[article_name] = mon_lat
-            lon[article_name] = mon_lon
 
-        result = re.match("\[\[([^\|]+?)\]\]", mon_name)
+        result = re.match("\[\[([^\|]+?)\]\]", aMonument.name)
         if (result and result.group(1)): 
             article_name = result.group(1)
-            lat[article_name] = mon_lat
-            lon[article_name] = mon_lon
         
         if (article_name):
             if article_name not in duplicateArticles:
                 if article_name in articleNames:
                     duplicateArticles.append(article_name)
                     articleNames.remove(article_name)
+                    for sMon in monumentsWithArticle:
+                        if (sMon.article == article_name):
+                            monumentsWithArticle.remove(sMon)
                 else:
                     articleNames.append(article_name)
+                    aMonument.article = article_name
+                    monumentsWithArticle.append(aMonument)
     
     if len(duplicateArticles):
         wikipedia.output(u'Multiple references to following articles: %s in monument lists! Skipped those.' % duplicateArticles[:])
     
-    for article in articleNames:
+    for aMonument in monumentsWithArticle:
         PageNs = WP_ARTICLE_NS
         followRedirect = True  
-        (retStatus, pageId, redirNs, redirTitle) = getPageId(article, connWiki, cursorWiki, PageNs, followRedirect)
+        (retStatus, pageId, redirNs, redirTitle) = getPageId(aMonument.article, connWiki, cursorWiki, PageNs, followRedirect)
         if (retStatus == 'FOLLOWED_REDIR' and redirNs == WP_ARTICLE_NS):
-            lat[redirTitle] = lat[article]
-            lon[redirTitle] = lon[article]
-            article = redirTitle
+            aMonument.article = redirTitle
         if (pageId):
             if not hasCoordinates(pageId, lang, cursorWiki):
-                addCoords(countrycode, lang, article, lat[article], lon[article], coordconfig)
+                addCoords(countrycode, lang, aMonument, coordconfig)
         
 
 
@@ -133,12 +147,22 @@ def getMonumentsWithCoordinates(countrycode, lang, cursor):
     '''
     Get monuments with coordinates from monuments database for a certain country/language combination.
     '''
-    result = {}
-    query = """SELECT name, lat, lon FROM monuments_all 
+    result = []
+    query = """SELECT id, name, lat, lon, source FROM monuments_all 
                    WHERE lat<>0 AND lon<>0 AND country=%s AND lang=%s"""
     cursor.execute(query, (countrycode, lang))
 
-    result = cursor.fetchall ()
+    #result = cursor.fetchall ()
+    while True:
+        try:
+            row = cursor.fetchone()
+            aMon = Monument()
+            aMon.country = countrycode
+            aMon.wikilang = lang
+            (aMon.id, aMon.name, aMon.lat, aMon.lon, aMon.source) = row           
+            result.append(aMon)
+        except TypeError:
+            break    
 
     return result
 
@@ -152,7 +176,8 @@ def hasCoordinates(pageId, lang, cursor):
 
         # check if primary coordinate i.e. article coordinate exists for pageId
         query = """SELECT gc_from FROM %s
-                      WHERE gc_from = %s AND gc_primary = 1""" 
+                      WHERE (gc_from = %s AND gc_primary = 1)
+                      LIMIT 1""" 
         # FIXME escape & sanitize coordTable and pageId
         cursor.execute(query % (coordTable, int(pageId) ))
 
@@ -168,13 +193,14 @@ def getPageId(pageName, conn, cursor, pageNamespace = WP_ARTICLE_NS, followRedir
     get Wikipedia pagename pageId
     '''
     
-    #underscores
+    #underscores   
     pageName = pageName.replace(u' ', u'_')
     retStatus = ''
     pageId = ''
     redirNs = ''
     redirTitle = u''
 
+    #FIXME page_titles like 'Château_de_Bercy' won't work, but titles like 'Käru' do ??
     query = """SELECT page_id, page_is_redirect FROM page 
                    WHERE page_namespace = %s AND page_title = %s"""
     cursor.execute(query, (pageNamespace, pageName))
@@ -189,7 +215,7 @@ def getPageId(pageName, conn, cursor, pageNamespace = WP_ARTICLE_NS, followRedir
             if (followRedirect):
                 (redirNs, redirTitle) = getRedirPageNsTitle(pageId, cursor)
                 redirTitle = unicode(redirTitle, "utf-8")
-                (retStatus, pageId, dummy1, dummy2) = getPageId(redirTitle, conn, cursor, redirNs)
+                (dummy0, pageId, dummy1, dummy2) = getPageId(redirTitle, conn, cursor, redirNs)
                 retStatus = 'FOLLOWED_REDIR'
             else:
                 retStatus = 'REDIRECT'
@@ -203,64 +229,71 @@ def getRedirPageNsTitle(pageId, cursor):
     Get redirect page namespace and title.
     '''
 
-    pageNs = ''
-    pageTitle = u''
+    if (pageId):
+        pageNs = ''
+        pageTitle = u''
     
-    query = """SELECT rd_namespace, rd_title FROM redirect
+        query = """SELECT rd_namespace, rd_title FROM redirect
                 WHERE rd_from = %s"""
-    cursor.execute(query, (pageId,))
+        cursor.execute(query, (pageId,))
 
-    if (cursor.rowcount > 0):
-        row = cursor.fetchone()
-        (pageNs, pageTitle) = row
+        if (cursor.rowcount > 0):
+            row = cursor.fetchone()
+            (pageNs, pageTitle) = row
     
-    return (pageNs, pageTitle)
+        return (pageNs, pageTitle)
     
     
-def addCoords(countrycode, lang, article, lat, lon, coordconfig):
+def addCoords(countrycode, lang, monument, coordconfig):
     '''
     Add the coordinates to article.
     '''
 
-    coordTemplate = coordconfig.get('coordTemplate')
-    coordTemplateSyntax = coordconfig.get('coordTemplateSyntax')
-    site = wikipedia.getSite(lang, 'wikipedia')
+    if (countrycode and lang):
+        coordTemplate = coordconfig.get('coordTemplate')
+        coordTemplateSyntax = coordconfig.get('coordTemplateSyntax')
+        site = wikipedia.getSite(lang, 'wikipedia')
 
-    page = wikipedia.Page(site, article)
-    try:
-        text = page.get()
-    except wikipedia.NoPage: # First except, prevent empty pages
-        return False
-    except wikipedia.IsRedirectPage: # second except, prevent redirect
-        wikipedia.output(u'%s is a redirect!' % article)
-        return False
-    except wikipedia.Error: # third exception, take the problem and print
-        wikipedia.output(u"Some error, skipping..")
-        return False       
+        page = wikipedia.Page(site, monument.article)
+        try:
+            text = page.get()
+        except wikipedia.NoPage: # First except, prevent empty pages
+            return False
+        except wikipedia.IsRedirectPage: # second except, prevent redirect
+            wikipedia.output(u'%s is a redirect!' % monument.article)
+            return False
+        except wikipedia.Error: # third exception, take the problem and print
+            wikipedia.output(u"Some error, skipping..")
+            return False       
     
-    if coordTemplate in page.templates():
-        return False
+        if coordTemplate in page.templates():
+            return False
 
-    newtext = text
-    replCount = 1
-    coordText = coordTemplateSyntax % (lat, lon, countrycode.upper() )
-    localCatName = wikipedia.getSite().namespace(WP_CATEGORY_NS)
-    catStart = r'\[\[(' + localCatName + '|Category):'
-    catStartPlain = u'[[' + localCatName + ':'
-    replacementText = u''
-    replacementText = coordText + '\n\n' + catStartPlain
+        newtext = text
+        replCount = 1
+        coordText = coordTemplateSyntax % (monument.lat, monument.lon, countrycode.upper() )
+        localCatName = wikipedia.getSite().namespace(WP_CATEGORY_NS)
+        catStart = r'\[\[(' + localCatName + '|Category):'
+        catStartPlain = u'[[' + localCatName + ':'
+        replacementText = u''
+        replacementText = coordText + '\n\n' + catStartPlain
     
-    # insert coordinate template before categories
-    newtext = re.sub(catStart, replacementText, newtext, replCount, flags=re.IGNORECASE)
+        # insert coordinate template before categories
+        newtext = re.sub(catStart, replacementText, newtext, replCount, flags=re.IGNORECASE)
 
-    if text != newtext:
-        comment = u'Adding template %s based on data from monuments list' % (coordTemplate,)
-        wikipedia.showDiff(text, newtext)
-        page.put(newtext, comment)
-        return True
+        if text != newtext:
+            wikilist = u''
+            matchWikipage = re.match("title=(.+?)&", monument.source)
+            if (matchWikipage and matchWikipage.group(1)): 
+                wikilist = matchWikipage.group(1)
+            comment = u'Adding template %s based on [[%s]], # %s' % (coordTemplate, wikilist, monument.id)
+            wikipedia.showDiff(text, newtext)
+##FIXME            page.put(newtext, comment)
+            return True
+        else:
+            return False
     else:
         return False
-    
     
 def main():
     countrycode = u''
