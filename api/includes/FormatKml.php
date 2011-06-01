bf756b5f36367b410f4a5f8a842873893b1d280b
kentaur <kentaur@3e45a915-b2e0-c146-c98e-857251466b57>
2011-06-01 03:38:11 +0000

    kml test
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@164 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ api/includes/FormatKml.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Kml output type
+ */
+class FormatXml extends FormatBase {
+	function getContentType() {
+		return "application/application/vnd.google-earth.kml+xml";
+	}
+	
+	function headers() {
+		parent::headers();
+		echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+	}
+	
+	function outputBegin() {
+		echo '<kml xmlns="http://www.opengis.net/kml/2.2">';
+        echo '<Document>
+        <Style id="monumentStyle"><IconStyle id="monumentIcon"><Icon><href>http://maps.google.com/mapfiles/kml/paddle/red-blank.png</href></Icon></IconStyle></Style>
+        <Style id="monPicStyle"><IconStyle id="monPicIcon"><Icon><href>http://maps.google.com/mapfiles/kml/paddle/red-circle.png</href></Icon></IconStyle></Style>';
+	}
+	
+	function outputRow($row, $selectedItems) {
+		echo '<Placemark';
+        $placemarkId = $row['country'] . $row['lang'] . $row['id'];
+        echo ' id="'. htmlspecialchars( $placemarkId ) .'">';
+        echo '<name>' . htmlspecialchars( $row['name'] ) . '</name>';
+        echo '<description>';
+        //echo '<![CDATA[';
+		foreach ( $row as $name => $value ) {
+			if ( in_array( $name, $selectedItems ) ) {
+				echo ' ' . htmlspecialchars( $name ) . ' -- ' . htmlspecialchars( $value ) . '';
+			}
+		}
+        //echo ']]>';
+		echo '</description>';
+        if ($row['image']) {
+            $styleUrl = '#monPicStyle';
+        } else {
+            $styleUrl = '#monumentStyle';
+        }
+        echo '<styleUrl>' . $styleUrl . '</styleUrl>';
+        echo '<Point><coordinates>' . $row['lon'] . ','  . $row['lat'] . '</coordinates></Point>';
+		echo '</Placemark>';
+	}
+	function outputEnd() {
+		 echo '</Document>';
+		echo '</kml>';
+	}
+}
