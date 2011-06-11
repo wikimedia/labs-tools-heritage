254a58b44be6a79339c24f709648f6f75e98be17
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-06-11 15:47:41 +0000

    Going to English names. Have encoding problems.
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@200 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/sql/create_table_monuments_by_(be-x-old).sql
+++ erfgoedbot/sql/create_table_monuments_by_(be-x-old).sql
@@ -1,20 +1,20 @@
-﻿/* Create table statement for Belarus in Belarussian */
+/* Create table statement for Belarus in Belarussian */
 connect p_erfgoed_p sql.toolserver.org;
 DROP TABLE IF EXISTS `monuments_by_(be-x-old)`;
 CREATE TABLE `monuments_by_(be-x-old)` (
-  `шыфр` int(8) NOT NULL DEFAULT '0',
-  `назва` varchar(255) NOT NULL DEFAULT '',
-  `датаваньне` varchar(255) NOT NULL DEFAULT '',
-  `населены_пункт` varchar(255) NOT NULL DEFAULT '',
-  `адрэса` varchar(255) NOT NULL DEFAULT '',
-  `катэгорыя ` varchar(255) NOT NULL DEFAULT '',
-  `шырата` double NOT NULL DEFAULT '0',
-  `даўгата` double NOT NULL DEFAULT '0',
-  `каардынаты` varchar(255) NOT NULL DEFAULT '',
-  `выява` varchar(255) NOT NULL DEFAULT '',
+  `id` int(8) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `date` varchar(255) NOT NULL DEFAULT '',
+  `place` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `category` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `coordinates` varchar(255) NOT NULL DEFAULT '',
+  `image` varchar(255) NOT NULL DEFAULT '',
   `source` varchar(255) NOT NULL DEFAULT '',
   `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`шыфр`),
-  KEY `latitude` (`шырата`),
-  KEY `longitude` (`даўгата`)
+  PRIMARY KEY (`id`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
