2172ba91a2a2888c13ed7c2380dfda022f53a412
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-06-11 12:46:47 +0000

    Another table
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@193 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_be-wal_(nl).sql
@@ -0,0 +1,23 @@
+/* Create table statement for the Beschermd Erfgoed in Wallonië in Dutch */
+connect p_erfgoed_p sql.toolserver.org;
+DROP TABLE IF EXISTS `monuments_wal-vlg_(nl)`;
+CREATE TABLE `monuments_wal-vlg_(nl)` (
+  `niscode` int(8) NOT NULL DEFAULT '0',
+  `objcode` varchar(15) NOT NULL DEFAULT '0',
+  `descr_nl` varchar(255) NOT NULL DEFAULT '',
+  `descr_fr` varchar(255) NOT NULL DEFAULT '',
+  `gemeente` varchar(255) NOT NULL DEFAULT '',
+  `deelgem` varchar(255) NOT NULL DEFAULT '',
+  `adres` varchar(255) NOT NULL DEFAULT '',
+  `objtype` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `architect` varchar(255) NOT NULL DEFAULT '',
+  `bouwjaar`  varchar(255) NOT NULL DEFAULT '',
+  `image` varchar(255) NOT NULL DEFAULT '',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`niscode`, `objcode`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
