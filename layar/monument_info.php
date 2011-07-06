0438e0b684bc748de8da28631ca8f2a1deb956e4
kentaur <kentaur@3e45a915-b2e0-c146-c98e-857251466b57>
2011-07-06 18:40:54 +0000

    add registrant url
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@217 3e45a915-b2e0-c146-c98e-857251466b57

--- layar/monument_info.php
+++ layar/monument_info.php
@@ -13,6 +13,22 @@ require dirname( dirname( __FILE__ ) ) . '/api/autoloader.php';
 require dirname( dirname( dirname( __FILE__ ) ) ) . '/database.inc';
 require_once( dirname( dirname( __FILE__ ) ) . '/api/includes/CommonFunctions.php' );
 
+//uses id field from monuments_all table
+function getRegistrantUrl($lang, $monumentId) {
+	$registrant_url = array(
+	"et" => "http://register.muinas.ee/?menuID=monument&action=view&id=", 
+	"fr" => "http://www.culture.gouv.fr/public/mistral/merimee_fr?ACTION=CHERCHER&FIELD_1=REF&VALUE_1=",
+	"nl" => "http://www.kich.nl/kich2010/rapport.jsp?id_qualifier=ODB:Rijksmonumentnr&id="
+	);
+	$outUrl = '';
+	
+	if ( isset($registrant_url[$lang] ) ) {
+		$outUrl = $registrant_url[$lang] . $monumentId;
+		return $outUrl;
+	}
+	
+	return $outUrl;
+}
 
 //main
 
@@ -67,7 +83,12 @@ if (isset($_GET["country"]) AND isset($_GET["lang"]) AND isset($_GET["id"]) ) {
 			if ($row->lat and $row->lon) {
 				$desc .= '<li>location: ' . $row->lat . ', ' . $row->lon . '</li>';
 			}
-			$desc .= '<li>id: ' . $row->id. '</li>';
+			$registrantUrl = getRegistrantUrl($row->lang, $row->id);
+			if ($registrantUrl) {
+				$desc .= '<li>id: <a href="' . $registrantUrl . '">' . $row->id . '</a></li>';
+			} else {
+				$desc .= '<li>id: ' . $row->id . '</li>';
+			}
 			$desc .= '<li>country: ' . strtoupper($row->country). '</li>';
 			if (preg_match("/^(.+?)&/", $row->source, $matches) ) { 
 				$wikiListUrl = $matches[1];
