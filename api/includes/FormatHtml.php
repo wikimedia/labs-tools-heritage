5e032fd5272ada7553dd4f270730c6a440eced1b
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2011-07-26 18:04:03 +0000

    Spaces to tabs
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@244 3e45a915-b2e0-c146-c98e-857251466b57

--- api/includes/FormatHtml.php
+++ api/includes/FormatHtml.php
@@ -11,28 +11,28 @@ class FormatHtml extends FormatBase {
 	
 	function headers() {
 		parent::headers();
-                echo '<!DOCTYPE HTML
-    PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-     "http://www.w3.org/TR/html4/loose.dtd">';
+		echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
 	}
-        function linebreak() {
-                echo "\n";
-        }
+	
+	function linebreak() {
+		echo "\n";
+	}
 
-        private $isfirstrow = 0;
+	private $isfirstrow = 0;
 	
 	function outputBegin() {
 		echo '<html>';$this->linebreak();
-                echo '<head>';$this->linebreak();
-                echo '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">';$this->linebreak();
-                echo '<style type="text/css">';$this->linebreak();
-                echo 'td, th,table { border: 1px solid gray; border-collapse: collapse; }';$this->linebreak();
-                echo 'th { background:steelblue; }';$this->linebreak();
-                echo 'tr{ background:lightsteelblue; opacity:0.8; }';$this->linebreak();
-                echo 'tr:hover { opacity:0.99; }';$this->linebreak();
-                echo 'tr#header { opacity:0.99; }';$this->linebreak();
-                echo "</style>\n</head>\n<body>\n<table>\n";
+		echo '<head>';$this->linebreak();
+		echo '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">';$this->linebreak();
+		echo '<style type="text/css">';$this->linebreak();
+		echo 'td, th,table { border: 1px solid gray; border-collapse: collapse; }';$this->linebreak();
+		echo 'th { background:steelblue; }';$this->linebreak();
+		echo 'tr{ background:lightsteelblue; opacity:0.8; }';$this->linebreak();
+		echo 'tr:hover { opacity:0.99; }';$this->linebreak();
+		echo 'tr#header { opacity:0.99; }';$this->linebreak();
+		echo "</style>\n</head>\n<body>\n<table>\n";
 	}
+	
 	function outputContinue($row, $continueKey, $primaryKey) {
 		$continue = '';
 		foreach ( $primaryKey as $key ) {
@@ -43,22 +43,23 @@ class FormatHtml extends FormatBase {
 	}
 	
 	function outputRow($row, $selectedItems) {
-                $this->selectedCount = count($selectedItems);
+		$this->selectedCount = count($selectedItems);
 		if (($this->isfirstrow)>=count($selectedItems)) { echo '<tr>'; }
-                else { echo '<tr id="header">'; }
-                $this->linebreak();
+		else { echo '<tr id="header">'; }
+		$this->linebreak();
 		foreach ( $row as $name => $value ) {
-                     if (($this->isfirstrow)>=count($selectedItems)) {
-			if ( in_array( $name, $selectedItems ) ) {
-				echo '<td>' . htmlspecialchars( $value ) . '</td>';$this->linebreak();
-			}
-                     } else {
-                        $this->isfirstrow += 1;
-                        echo '<th>' . $name . '</th>';$this->linebreak();
-                     }
+			if (($this->isfirstrow)>=count($selectedItems)) {
+				if ( in_array( $name, $selectedItems ) ) {
+					echo '<td>' . htmlspecialchars( $value ) . '</td>';$this->linebreak();
+				}
+		     } else {
+				$this->isfirstrow += 1;
+				echo '<th>' . $name . '</th>';$this->linebreak();
+		     }
 		}
 		echo '</tr>';$this->linebreak();
 	}
+	
 	function outputEnd() {
 		echo "</table>\n</body>\n</html>";
 	}
