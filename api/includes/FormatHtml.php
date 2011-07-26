7c9e20f4ff0beb3bc11a21244eba7cdb9d5edbc7
jcreus <jcreus@3e45a915-b2e0-c146-c98e-857251466b57>
2011-07-26 14:58:45 +0000

    Adding HTML output, still working.
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@224 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ api/includes/FormatHtml.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * HTML output type, based on XML
+ * @author Joancreus (jcreus), based on Platonides work 
+ */
+class FormatXml extends FormatBase {
+	function getContentType() {
+		return "text/html";
+	}
+	
+	function headers() {
+		parent::headers();
+		echo '<!DOCTYPE HTML
+    PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+     "http://www.w3.org/TR/html4/loose.dtd">';
+	}
+
+        private $isfirstrow = true;
+	
+	function outputBegin() {
+		echo '<html>';
+	}
+	function outputContinue($row, $continueKey, $primaryKey) {
+		$continue = '';
+		foreach ( $primaryKey as $key ) {
+			$continue .= "|" . rawurlencode( $row->$key );
+		}
+		$continue = substr( $continue, 1 );
+		//echo '<continue ' . $continueKey . '="' . htmlspecialchars( $continue ) . '" />';
+	}
+	
+	function outputRow($row, $selectedItems) {
+		echo '<tr>';
+		foreach ( $row as $name => $value ) {
+                     if (!$this->isfirstrow) {
+			if ( in_array( $name, $selectedItems ) ) {
+				echo '<td>' . htmlspecialchars( $value ) . '</td>';
+			}
+                     } else {
+                        $this->isfirstrow = false;
+                        echo '<th>' . $name . '</th>';
+                     }
+		}
+		echo '</tr>';
+	}
+	function outputEnd() {
+		echo '</body>\n</html>';
+	}
+}
