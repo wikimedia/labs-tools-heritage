47ec396e3b9ddfeb159fc898202f493c2cc2c0e9
jcreus <jcreus@3e45a915-b2e0-c146-c98e-857251466b57>
2011-07-27 13:29:36 +0000

    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@260 3e45a915-b2e0-c146-c98e-857251466b57

--- api/tripplanner/index.php
+++ api/tripplanner/index.php
@@ -4,17 +4,25 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script> 
 <script type="text/javascript">
 function newfield() {
-   return '<div class="field"><select name="type"><option value="incity">In the city</option><option value="specific">Specific monument</option></select><input type="text"><input value="+" type="button" onClick="javascript:$(\'#box\').append(newfield());">';
+   return '<div class="field"><select name="type"><option value="incity">In the city</option><option value="specific">Specific monument</option></select><input type="text" class="textinput"><input value="Add another point" type="button" onClick="javascript:$(\'#box\').append(newfield());"></div>';
 }
 
 $(document).ready(function () {
   $("#box").append(newfield());
+  $("#submit").click(function () {
+      $(".field").each(function (){
+         alert($(this).find(".textinput").val());
+      });
+  });
 });
 </script>
 </head>
 <body>
 <div id="box">
 
+</div>
+<div id="submit">
+<input id="submit" type="button" value="Submit">
 </div>
 </body>
 </html>
