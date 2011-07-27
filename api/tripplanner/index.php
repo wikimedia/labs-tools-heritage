e3e80a2d093b911e1bb095afc1258a67e912f50b
jcreus <jcreus@3e45a915-b2e0-c146-c98e-857251466b57>
2011-07-27 09:36:10 +0000

    Adding the trip planner
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@256 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ api/tripplanner/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Trip Planner - Wiki Loves Monuments</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script> 
+<script type="text/javascript">
+function newfield() {
+   return '<div class="field"><select name="type"><option value="incity">In the city</option></select><input type="text"><input type="button" onClick="javascript:$("#box").append(newfield());">';
+}
+
+$(document).ready(function () {
+  $("#box").append(newfield());
+});
+</script>
+</head>
+<body>
+<div id="box">
+
+</div>
+</body>
+</html>
