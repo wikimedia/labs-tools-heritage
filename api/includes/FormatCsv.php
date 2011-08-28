9815a93f8771fb56c48348b194057d9fe5612e8c
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2011-08-28 21:46:56 +0000

    Add basic CSV output
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@308 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ api/includes/FormatCsv.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Csv output type
+ * @author Platonides
+ */
+class FormatXml extends FormatBase {
+	function getContentType() {
+		return "text/csv";
+	}
+	
+	function headers() {
+		parent::headers();
+	}
+	
+	function outputBegin() {
+
+	}
+	
+	function outputContinue($row, $continueKey, $primaryKey) {
+		$continue = '';
+		foreach ( $primaryKey as $key ) {
+			$continue .= "|" . rawurlencode( $row->$key );
+		}
+		$continue = substr( $continue, 1 );
+		
+		echo '>> Continue at ' . $this->api->getUrl( array( $continueKey => $continue );
+	}
+	
+	function outputRow($row, $selectedItems) {
+		$needComma = false;
+		foreach ( $row as $name => $value ) {
+			if ( in_array( $name, $selectedItems ) ) {
+				if ( $needComma ) echo ',';
+				echo '"' . str_replace( '"', '""', $value ) . '"';
+				$needComma = true;
+			}
+		}
+	}
+	
+	function outputEnd() {
+
+	}
+}
