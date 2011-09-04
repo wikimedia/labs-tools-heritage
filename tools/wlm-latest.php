f6aa5c06cd49e08018f304041026c74ad1627344
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-09-04 16:22:01 +0000

    Someone forgot to add this to svn.
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@346 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ tools/wlm-latest.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html><head>
+    <title></title>
+    <style>
+        #photos-wlm {
+            margin: 0;
+            padding: 0;
+        }
+
+        #photos-wlm li {
+            list-style-type: none;
+        }
+
+        #photos-wlm a {
+            float: left;
+            width: 150px;
+            height: 150px;
+            margin: 10px;
+            background-repeat: no-repeat;
+            background-position: center center;
+        }
+    </style>
+<body>
+
+<div id="photos-wlm">
+</div>
+
+<script>
+    var photosDiv = document.getElementById('photos-wlm');
+    var photos;
+
+    function jscallback(data) {
+        for (var i = 0, l = data.length; i < l; i++) {
+            var photo = data[i],
+                thumb = (photo.image + "/150px-.jpg").replace("commons/", "commons/thumb/");
+
+            var li = document.createElement( 'li' );
+            var a = document.createElement( 'a' );
+            a.href = photo.url;
+            a.target = '_top';
+            a.style.backgroundImage = 'url("' + thumb + '")';
+            li.appendChild(a);
+
+            photos.appendChild(li);
+        }
+    }
+
+    function loadPhotos() {
+        var s = document.createElement('script');
+        s.id = 'achterkamer';
+        s.src = 'http://toolserver.org/~erfgoed/tools/wlmlast.php?callback=jscallback<?php if (isset($_GET['country'])) echo '&country=' . htmlspecialchars(urlencode($_GET['country'])); ?>';
+        document.getElementsByTagName("head")[0].appendChild(s);
+    }
+    if (photosDiv) {
+        photos = document.createElement('ul');
+        photosDiv.appendChild(photos);
+        loadPhotos();
+    }
+</script>
+
+</body></html>
