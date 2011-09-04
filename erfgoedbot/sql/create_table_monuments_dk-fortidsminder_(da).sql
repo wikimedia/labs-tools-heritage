46ba2fff5df4f7310115a05dba99e8bd1ddbf6e9
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2011-09-04 16:35:57 +0000

    Another source from Denmark.
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@350 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_dk-fortidsminder_(da).sql
@@ -0,0 +1,21 @@
+/* Create table statement for fortidsminder in Denmark in Danish */
+connect p_erfgoed_p sql.toolserver.org;
+DROP TABLE IF EXISTS `monuments_dk-fortidsminder_(da)`;
+CREATE TABLE `monuments_dk-fortidsminder_(da)` (
+  `fredningsnummer` int(11) NOT NULL DEFAULT '0',
+  `stednavn` varchar(255) NOT NULL DEFAULT '',
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `datering` varchar(255) NOT NULL DEFAULT '',
+  `sevaedighed` varchar(255) NOT NULL DEFAULT '',
+  `systemnummer` int(11) NOT NULL DEFAULT '0',
+  `anlnr` int(11) NOT NULL DEFAULT '0',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `billede` varchar(255) NOT NULL DEFAULT '',
+  `bemaerkning` varchar(255) NOT NULL DEFAULT '',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`fredningsnummer`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
