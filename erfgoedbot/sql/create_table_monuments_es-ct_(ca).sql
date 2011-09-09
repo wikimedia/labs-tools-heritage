67efefe86ad002b562dc4419ebfeddc90d9cfe1c
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2011-09-09 14:34:44 +0000

    idurl can be text, such as bcn/01/1097 or diba/209000213
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@387 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/sql/create_table_monuments_es-ct_(ca).sql
+++ erfgoedbot/sql/create_table_monuments_es-ct_(ca).sql
@@ -3,7 +3,7 @@ connect p_erfgoed_p sql.toolserver.org;
 DROP TABLE IF EXISTS `monuments_es-ct_(ca)`;
 CREATE TABLE `monuments_es-ct_(ca)` (
   `bic` varchar(25) NOT NULL DEFAULT '0',
-  `idurl` int(11) NOT NULL DEFAULT '0',
+  `idurl` varchar(255) NOT NULL DEFAULT '',
   `bcin` varchar(255) NOT NULL DEFAULT '0',
   `nom` varchar(255) NOT NULL DEFAULT '',
   `estil` varchar(255) NOT NULL DEFAULT '',
