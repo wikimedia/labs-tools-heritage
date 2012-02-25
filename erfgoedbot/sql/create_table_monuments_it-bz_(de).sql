8c0466b35a03ba233136e8fd9df9c3f198ae5c2c
cirdan <cirdan@3e45a915-b2e0-c146-c98e-857251466b57>
2012-02-25 14:35:44 +0000

    forgot to add create table file
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@522 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_monuments_it-bz_(de).sql
@@ -0,0 +1,27 @@
+/* Create table statement for the Monuments in Austria in German */
+connect p_erfgoed_p sql.toolserver.org;
+DROP TABLE IF EXISTS `monuments_it-bz_(de)`;
+CREATE TABLE `monuments_it-bz_(de)` (
+  `objektid` varchar(11) NOT NULL DEFAULT '0',
+  `foto` varchar(255) NOT NULL DEFAULT '',
+  `fotobeschreibung` varchar(255) NOT NULL DEFAULT '',
+  `commonscat` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `artikel` varchar(255) NOT NULL DEFAULT '',
+  `anzeige-Name` varchar(255) NOT NULL DEFAULT '',
+  `adresse` varchar(255) NOT NULL DEFAULT '',
+  `adresse-sort` varchar(255) NOT NULL DEFAULT '',
+  `region-iso` varchar(255) NOT NULL DEFAULT '',
+  `katastralgemeinde` varchar(255) NOT NULL DEFAULT '',
+  `status` varchar(255) NOT NULL DEFAULT '',
+  `beschreibung` varchar(255) NOT NULL DEFAULT '',
+  `eintragung` varchar(255) NOT NULL DEFAULT '',
+  `beschluss` varchar(255) NOT NULL DEFAULT '',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ObjektID`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
