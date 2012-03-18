e4070706d989aba502ced7b2bc60853301ec9107
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2012-03-18 12:53:53 +0000

    Enable i18n for the api. More info at https://wiki.toolserver.org/view/Intuition
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@567 3e45a915-b2e0-c146-c98e-857251466b57

--- api/api.php
+++ api/api.php
@@ -4,8 +4,17 @@
 require dirname( __FILE__ ) . '/autoloader.php';
 require dirname( dirname( dirname( __FILE__ ) ) ) . '/database.inc';
 
+/* Localization */
+require_once( '/home/project/i/n/t/intuition/ToolserverI18N/ToolStart.php' );
+
 Database::define(Monuments::$dbServer, Monuments::$dbDatabase, 
 	Monuments::$dbUser, $toolserver_password );
+
+$opts = array(
+    'domain' => 'MonumentsAPI', // name of your main text-domain here
+    'globalfunctions' => true, // defines _(), _e() and _g() as shortcut for $I18N->msg( .. )
+);
+$I18N = new TsIntuition( $opts );
 
 $api = new ApiMonuments();
 $api->execute();
