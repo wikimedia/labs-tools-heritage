05e6c28ae8cb49bfea15d70444568a96841ec11d
platonides <platonides@3e45a915-b2e0-c146-c98e-857251466b57>
2012-05-10 22:38:48 +0000

    Commit script to generate table definition files from monuments_config.py The sql files are now identical to the ones autogenerated, so they can be removed.
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@665 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/monument_tables.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+'''
+Create the monuments tables SQL from monuments_config.py
+
+Author: Platonides
+'''
+import sys, time, warnings
+import monuments_config as mconfig
+sys.path.append("/home/project/e/r/f/erfgoed/pywikipedia")
+#import wikipedia, MySQLdb, config, re, pagegenerators
+
+def processCountry(countrycode, lang, countryconfig):
+	extra_cols = "  `source` varchar(255) NOT NULL DEFAULT '',\n" "  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n" # These are not listed in the monument config
+	
+	table = countryconfig.get('table')
+	f = open("sql/create_table_" + table + ".sql","w")
+	f.write("connect p_erfgoed_p sql.toolserver.org;\n")
+	f.write("DROP TABLE IF EXISTS `" + table + "`;\n")
+	f.write("CREATE TABLE IF NOT EXISTS `" + table + "` (\n")
+	
+	source_primkey = countryconfig.get('primkey')
+	primkey = False
+	
+	for field in countryconfig.get('fields'):
+		column = field.get('dest')
+		if column == '': # An entry with no dest is intended to be skipped
+			continue
+		
+		if not primkey and source_primkey == field.get('source'): # primkey in config refers to the source name
+			primkey = column
+		
+		if column in ['lon', 'lat']:
+			f.write(b'  `' + column.encode('utf8') + b"` double NOT NULL DEFAULT  0,\n")
+		else:
+			if column == 'monument_article':
+				f.write(extra_cols)
+				extra_cols =''
+			type = field.get('type')
+			if not type:
+				type = "varchar(255) NOT NULL DEFAULT "
+				if field.get('default'):
+					type += "'" + field.get('default') + "'"
+				else:
+					type += "''"
+			elif type.startswith("int("):
+				type += " NOT NULL DEFAULT  0"
+			elif type.startswith("varchar("):
+				if field.get('default'):
+					type += " NOT NULL DEFAULT '" + field.get('default') + "'"
+				else:
+					type += " NOT NULL DEFAULT ''"
+				
+			f.write(b'  `' + column.encode('utf8') + b"` " + type + ",\n")
+	
+	f.write(extra_cols)
+	
+	if not primkey:
+		if not isinstance(source_primkey, (str, unicode)):
+			primkey = u"`,`".join(source_primkey)
+		else:
+			raise "Primary key not found"
+	
+	f.write('  PRIMARY KEY (`' + primkey.encode('utf8') + '`),\n')
+	f.write('  KEY `latitude` (`lat`),\n  KEY `longitude` (`lon`)\n')
+	f.write(') ENGINE=MyISAM DEFAULT CHARSET=utf8;\n')
+	f.close()
+
+def main():
+	for (countrycode, lang), countryconfig in mconfig.countries.iteritems():
+		processCountry(countrycode, lang, countryconfig)
+
+if __name__ == "__main__":
+	try:
+		main()
+	finally:
+		#wikipedia.stopme()
+		pass
