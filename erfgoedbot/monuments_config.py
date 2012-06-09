1bd1b67429b67bebcaf3bb2bc4ce4af2ccb6c9d5
kentaur <kentaur@3e45a915-b2e0-c146-c98e-857251466b57>
2012-06-09 18:32:52 +0000

    config for EE
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@689 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/monuments_config.py
+++ erfgoedbot/monuments_config.py
@@ -1428,6 +1428,10 @@ countries = {
                 'type' : 'int(11)',
             },
             {
+                'source' : u'maakond',
+                'dest' : u'maakond',
+            },
+            {
                 'source' : u'nimi',
                 'dest' : u'nimi',
             },
