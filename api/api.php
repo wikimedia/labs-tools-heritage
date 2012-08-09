b748a9eb84941a323b96a58237509f11ebb63234
maxsem <maxsem@3e45a915-b2e0-c146-c98e-857251466b57>
2012-08-09 16:48:25 +0000

    Allow disabling TsIntuition completely by setting $tsI18nDir to false
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@822 3e45a915-b2e0-c146-c98e-857251466b57

--- api/api.php
+++ api/api.php
@@ -6,15 +6,16 @@ require dirname( __FILE__ ) . '/includes/Defaults.php';
 require dirname( dirname( dirname( __FILE__ ) ) ) . '/database.inc';
 
 /* Localization */
-require_once( "$tsI18nDir/ToolStart.php" );
+if ( $tsI18nDir ) {
+	require_once( "$tsI18nDir/ToolStart.php" );
+	$opts = array(
+		'domain' => 'MonumentsAPI', // name of your main text-domain here
+		'globalfunctions' => true, // defines _(), _e() and _g() as shortcut for $I18N->msg( .. )
+	);
+	$I18N = new TsIntuition( $opts );
+}
 
 Database::define($dbServer, $dbDatabase, $dbUser,
 	isset( $toolserver_password )? $toolserver_password : $dbPassword );
-
-$opts = array(
-    'domain' => 'MonumentsAPI', // name of your main text-domain here
-    'globalfunctions' => true, // defines _(), _e() and _g() as shortcut for $I18N->msg( .. )
-);
-$I18N = new TsIntuition( $opts );
 
 ApiMain::dispatch();
