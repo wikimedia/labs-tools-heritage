6fe19a0377a585a71fac44a8b928aaf7c2fca1ec
maxsem <maxsem@3e45a915-b2e0-c146-c98e-857251466b57>
2012-08-28 17:24:53 +0000

    Log user-agent
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@906 3e45a915-b2e0-c146-c98e-857251466b57

--- api/includes/Debug.php
+++ api/includes/Debug.php
@@ -21,7 +21,12 @@ class Debug {
 			self::$debugLog = fopen( DEBUG_FILE, 'at' );
 			if ( isset( $_SERVER['REQUEST_URI'] ) ) {
 				$time = gmdate( DATE_ISO8601 );
-				fputs( self::$debugLog, "$time\t{$_SERVER['REQUEST_URI']}\n" );
+				$line = "$time\t{$_SERVER['REQUEST_URI']}";
+				if ( isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
+					$line .= "\t" . $_SERVER['HTTP_USER_AGENT'];
+				}
+				$line .= "\n";
+				fputs( self::$debugLog, $line );
 			}
 		}
 	}
