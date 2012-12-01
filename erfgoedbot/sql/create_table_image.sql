1dc2bd0a15bafbf6b9750721833941af44e96d96
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2012-12-01 15:09:58 +0000

    Starting work for the image table
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@1024 3e45a915-b2e0-c146-c98e-857251466b57

---
+++ erfgoedbot/sql/create_table_image.sql
@@ -0,0 +1,17 @@
+/* Create table for images with an identifier
+ *
+ *
+ * If you change something please test it.
+ */
+
+SET NAMES UTF8;
+
+DROP TABLE IF EXISTS `image`;
+
+CREATE TABLE `image` (
+	  `country` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+	  `id` varchar(25) NOT NULL DEFAULT '0',
+	  `img_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+	  PRIMARY KEY (`country`,`id`),
+	  KEY `img_name` (`img_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
