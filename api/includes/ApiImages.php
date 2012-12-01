70530521f023346deb0685fe07f09596cbca0cf0
multichill <multichill@3e45a915-b2e0-c146-c98e-857251466b57>
2012-12-01 19:33:04 +0000

    Got it working. Still some FIXME's
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@1031 3e45a915-b2e0-c146-c98e-857251466b57

--- api/includes/ApiImages.php
+++ api/includes/ApiImages.php
@@ -24,6 +24,12 @@ class ApiImages extends ApiBase {
 				ApiBase::PARAM_DFLT => false,
 				ApiBase::PARAM_TYPE => 'string',
 			),
+			'width' => array(
+				 ApiBase::PARAM_DFLT => 220,
+				 ApiBase::PARAM_TYPE => 'integer',
+				 ApiBase::PARAM_MIN => 1,
+				 ApiBase::PARAM_MAX => 9999999999,
+			),
 		);
 		$params = array_merge_recursive( $defaultParams, $params );
 		return $params;
@@ -46,22 +52,35 @@ class ApiImages extends ApiBase {
 	 *
 	 */
 	public function images() {
-		$display_fields = array( 'country', 'id', 'img_name');
+		$display_fields = array( 'country', 'id', 'img_name', 'img_thumb', 'img_page');
 		
-		// Escaping?
-		$country = $this->getParam( "country" )
-		$id = $this->getParam( "id" )
+		$country = $this->getParam( "country" );
+		$id = $this->getParam( "id" );
+		$width = $this->getParam( "width" );
+		$limit = $this->getParam( "limit" );
 		
 		$data = array();
 		$db = Database::getDb();
-		$fields = array( 'country', 'id' );
+		$fields = array( 'country', 'id', 'img_name' );
+
+		// FIXME: Escaping?
 		$where = array( 'country' => $country, 'id' => $id );
 		
-		$res = $db->select( $fields, 'images', $where );
+		$res = $db->select( $fields, 'image', $where );
 		while ( $row = $db->fetchAssoc( $res ) ) {
+			$this->imageLinksRow( $row, $width );
 			$data[] = $row;
 		}
-		$continueKey = null
+		// FIXME: Implement the paging
+		$continueKey = null;
 		$this->getFormatter()->output( $data, $limit, $continueKey, $display_fields, null );
+	}
+	/*
+	 * Add a link to a thumbnail and to the image page
+	 *
+	 */
+	private function imageLinksRow( &$row, $width ) {
+		$row['img_thumb'] = 'http://commons.wikimedia.org/w/thumb.php?f=' . $row['img_name'] . '&width=' . $width;
+		$row['img_page'] = 'https://commons.wikimedia.org/w/index.php?title=File:' . $row['img_name']; 
 	}
 }
