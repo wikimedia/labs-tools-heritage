3c083463782ff76bd628751b7f493fa08e332f95
alexxw <alexxw@3e45a915-b2e0-c146-c98e-857251466b57>
2013-08-25 17:07:54 +0000

    add hk-hb to big db
    git-svn-id: https://svn.toolserver.org/svnroot/p_erfgoed@1154 3e45a915-b2e0-c146-c98e-857251466b57

--- erfgoedbot/sql/fill_table_monuments_all.sql
+++ erfgoedbot/sql/fill_table_monuments_all.sql
@@ -1202,7 +1202,7 @@ SELECT 'gh' AS `country`,
         `monument_article` AS `monument_article`,
         '' AS `registrant_url` # Not available
 		FROM `monuments_gh_(en)`;
-/* Hong Kong in English */
+/* Hong Kong declared monuments in English */
 REPLACE INTO `monuments_all_tmp` (`country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`, `lon`, `lat_int`, `lon_int`, `image`, `commonscat`, `source`, `changed`, `monument_article`, `registrant_url` )
 SELECT 'hk' AS `country`,
        'en' AS `lang`,
@@ -1226,6 +1226,30 @@ SELECT 'hk' AS `country`,
         `monument_article` AS `monument_article`,
         '' AS `registrant_url` # Not available
 		FROM `monuments_hk_(en)`;
+/* Hong Kong historic buildings in English */
+REPLACE INTO `monuments_all_tmp` (`country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`, `lon`, `lat_int`, `lon_int`, `image`, `commonscat`, `source`, `changed`, `monument_article`, `registrant_url` )
+SELECT 'hk-hb' AS `country`,
+       'en' AS `lang`,
+		`id` AS `id`,
+		'hk' AS `adm0`,
+		NULL AS `adm1`,
+		`region` AS `adm2`,
+		`district` AS `adm3`,
+		NULL AS `adm4`,
+		`name` AS `name`,
+		`location` AS `address`,
+		'Hong Kong' AS `municipality`,
+		NULL AS `lat`,
+		NULL AS `lon`,
+		NULL AS `lat_int`,
+		NULL AS `lon_int`,
+		`image` AS `image`,
+		NULL AS `commonscat`,
+		`source` AS `source`,
+		`changed` AS `changed`,
+        `monument_article` AS `monument_article`,
+        '' AS `registrant_url` # Not available
+		FROM `monuments_hk-hb_(en)`;
 /* India in English */
 REPLACE INTO `monuments_all_tmp` (`country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`, `lon`, `lat_int`, `lon_int`, `image`, `commonscat`, `source`, `changed`, `monument_article`, `registrant_url` )
 SELECT 'in' AS `country`,
