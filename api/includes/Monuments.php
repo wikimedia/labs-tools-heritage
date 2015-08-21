79e3b476142bcd48c43f7666f24947d983063a87
tools.heritage <tools.heritage@tools-bastion-01.tools.eqiad.wmflabs>
2015-08-21 22:57:55 +0000

    Add commonscat to $dbFields in API/Monuments.php
    Change-Id: Ifebd0c618f42a7ecaf3210d35d3823ffcaf118ce
    Signed-off-by: Jean-Fred <jeanfrederic.wiki@gmail.com>

--- api/includes/Monuments.php
+++ api/includes/Monuments.php
@@ -7,7 +7,7 @@ class Monuments {
 	static $dbTable = 'monuments_all';
 	
 	static $dbFields = array( 'country', 'lang', 'id', 'adm0', 'adm1', 'adm2', 'adm3', 'adm4', 'name', 'address', 'municipality', 
-'lat', 'lon', 'image', 'source', 'monument_article', 'registrant_url', 'changed' );
+'lat', 'lon', 'image', 'commonscat', 'source', 'monument_article', 'registrant_url', 'changed' );
 
 	static $dbPrimaryKey = array( 'country', 'lang', 'id' );
 }
