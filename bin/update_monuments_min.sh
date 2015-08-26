140904e2f26749f1fbf5828aac673f699b2154da
Jean-Fred <jeanfrederic.wiki@gmail.com>
2015-08-26 22:16:16 +0100

    Fix SQL command in update_monuments_min.sh bin script
    This wqas incorrect syntax
    
    Change-Id: Ib0fbdd7b49d8ed54b2ace8e71c2bcc4f4f73b547

--- bin/update_monuments_min.sh
+++ bin/update_monuments_min.sh
@@ -6,4 +6,4 @@
 /data/project/heritage/pywikibot/pwb.py /data/project/heritage/erfgoedbot/update_database.py -fullupdate
 
 # Update the all monuments table
-mysql -h tools-db s51138__heritage_p sql < /data/project/heritage/erfgoedbot/sql/fill_table_monuments_all.sql
+mysql -h tools-db s51138__heritage_p < /data/project/heritage/erfgoedbot/sql/fill_table_monuments_all.sql
