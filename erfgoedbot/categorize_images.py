wikimedia/labs-tools-heritage

commit 62a5fb386e18c1e82d618af862c79f84543b0b2c

Extract method get_new_categories
Change-Id: Ib0fdb90304a25d3e7e61ad8575575ddbbb2e2f5b

erfgoedbot/categorize_images.py
@@ -231,11 +231,38 @@ def categorizeImage(countrycode, lang, commonsTemplateName, commonsCategoryBase,
             u'Monument with id %s not in monuments database' % (monumentId, ))
         return False
 
+    newcats = get_new_categories(monumentId, monData, lang, commonsCatTemplates)
+
+    # See if one of the three options worked
+    if newcats:
+        oldtext = page.get()
+        for currentcat in currentcats:
+            if not currentcat.title(withNamespace=False) == commonsCategoryBase.title(withNamespace=False):
+                if currentcat.title(withNamespace=False) in oldtext:
+                    newcats.append(currentcat)
+
+        # Remove dupes
+        newcats = list(set(newcats))
+        if not set(currentcats) == set(newcats):
+            newtext = pywikibot.replaceCategoryLinks(oldtext, newcats)
+
+            comment = u'Adding categories based on [[Template:%s]] with identifier %s' % (
+                commonsTemplateName, monumentId)
+            pywikibot.showDiff(oldtext, newtext)
+            try:
+                page.put(newtext, comment)
+                return True
+            except pywikibot.EditConflict:
+                pywikibot.output(
+                    u'Got an edit conflict. Someone else beat me to it at %s' % page.title())
+    else:
+        pywikibot.output(u'Categories not found for %s' % page.title())
+
+
+def get_new_categories(monumentId, monData, lang, commonsCatTemplates):
     (monumentName, monumentCommonscat,
      monumentArticleTitle, monumentSource) = monData
-
     newcats = []
-
     # First try to add a category based on the commonscat field in the list
     if monumentCommonscat:
         # Might want to include some error checking here
@@ -278,31 +305,6 @@ def categorizeImage(countrycode, lang, commonsTemplateName, commonsCategoryBase,
         if monumentList.isRedirectPage():
             monumentList = monumentList.getRedirectTarget()
         newcats = getCategories(monumentList, commonsCatTemplates)
-
-    # See if one of the three options worked
-    if newcats:
-        oldtext = page.get()
-        for currentcat in currentcats:
-            if not currentcat.title(withNamespace=False) == commonsCategoryBase.title(withNamespace=False):
-                if currentcat.title(withNamespace=False) in oldtext:
-                    newcats.append(currentcat)
-
-        # Remove dupes
-        newcats = list(set(newcats))
-        if not set(currentcats) == set(newcats):
-            newtext = pywikibot.replaceCategoryLinks(oldtext, newcats)
-
-            comment = u'Adding categories based on [[Template:%s]] with identifier %s' % (
-                commonsTemplateName, monumentId)
-            pywikibot.showDiff(oldtext, newtext)
-            try:
-                page.put(newtext, comment)
-                return True
-            except pywikibot.EditConflict:
-                pywikibot.output(
-                    u'Got an edit conflict. Someone else beat me to it at %s' % page.title())
-    else:
-        pywikibot.output(u'Categories not found for %s' % page.title())
 
 
 def getMonData(countrycode, lang, monumentId, conn, cursor):

commit 1001ec3c682155ce9dc4c00d3913d4254b630ee7

Fix bug introduced in 62a5fb38
The extracted method was not returning the categories...

Change-Id: Ic9094cff08fd8af1cd8992bc960a3d1301a8c401

erfgoedbot/categorize_images.py
@@ -299,6 +299,7 @@ def get_new_categories(monumentId, monData, lang, commonsCatTemplates):
         if monumentList.isRedirectPage():
             monumentList = monumentList.getRedirectTarget()
         newcats = getCategories(monumentList, commonsCatTemplates)
+    return newcats
 
 
 def replace_default_cat_with_new_categories_in_image(page, commonsCategoryBase, newcats, comment):