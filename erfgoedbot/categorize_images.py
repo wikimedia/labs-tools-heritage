wikimedia/labs-tools-heritage

commit 44e7b5f370118166821e45c513b02c0ea94037fa

Make sure we do not add the base category when categorizing
Change-Id: I1fbe131efb7f5d26a01d746d035b500d8c6e5440

erfgoedbot/categorize_images.py
@@ -306,10 +306,15 @@ def get_new_categories(monumentId, monData, lang, commonsCatTemplates):
 
 def replace_default_cat_with_new_categories_in_image(page, commonsCategoryBase, newcats, comment):
     oldtext = page.get()
-    # In any case we remove the base category
-    newtext = textlib.replaceCategoryInPlace(oldtext, commonsCategoryBase, None)
     # Remove dupes
     newcats_set = set(newcats)
+    # Make sure we do not add the base category
+    newcats_set = newcats_set - set(commonsCategoryBase)
+    if len(newcats_set):
+        # No categories to add. We do not want to remove the base one
+        return False
+    # In any case we remove the base category
+    newtext = textlib.replaceCategoryInPlace(oldtext, commonsCategoryBase, None)
     # Make sure we do not add categories that were already there
     commons_site = pywikibot.Site(u'commons', u'commons')
     currentcats_set = set(textlib.getCategoryLinks(oldtext, site=commons_site))

commit be6bc79582ce903a7bbb965c154f4d7cc53b8af2

Fix bug introduced in 44e7b5f3
Change-Id: I3128ee8a0eb7521a78da1a58fb2ee790520b00d4

erfgoedbot/categorize_images.py
@@ -309,7 +309,7 @@ def replace_default_cat_with_new_categories_in_image(page, commonsCategoryBase, 
     # Remove dupes
     newcats_set = set(newcats)
     # Make sure we do not add the base category
-    newcats_set = newcats_set - set(commonsCategoryBase)
+    newcats_set = newcats_set - set([commonsCategoryBase])
     if len(newcats_set):
         # No categories to add. We do not want to remove the base one
         return False