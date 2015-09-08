wikimedia/labs-tools-heritage

commit 50e3ce9d2b1ab36bc4130994850c9a87f7d281e5

Extract method is_template_present_in_page
And use it throughout the module.

This method will check if a template is directly transcluded
in a given page. It will not rely on page.templates(), as this
method returns all templates transcluded, including via other templates.

Bug: T111757
Change-Id: I35774eea6b74afdb4a6ff4a9639712b94b45ceaa

erfgoedbot/categorize_images.py
@@ -283,7 +283,7 @@ def get_new_categories(monumentId, monData, lang, commonsCatTemplates):
                 try:
                     for commonsCatTemplateName in commonsCatTemplates:
                         commonsCatTemplate = pywikibot.Page(project_site, 'Template:%s' % commonsCatTemplateName)
-                        if commonsCatTemplate in monumentArticle.templates():
+                        if is_template_present_in_page(commonsCatTemplate, monumentArticle.templates):
                             newcats = []
                             newcats.append(
                                 getCategoryFromCommonscat(monumentArticle, commonsCatTemplates))
@@ -385,6 +385,11 @@ def getList(lang, monumentSource):
         return False
 
 
+def is_template_present_in_page(template, page):
+    """Return whether a template is present in a page (as in directly transcluded.)"""
+    return template in [x[0] for x in page.templatesWithParams()]
+
+
 def getCategories(page, commonsCatTemplates):
     '''
     Get Commons categories based on page.
@@ -395,7 +400,7 @@ def getCategories(page, commonsCatTemplates):
     result = set()
     for commonsCatTemplateName in commonsCatTemplates:
         commonsCatTemplate = pywikibot.Page(page.site, 'Template:%s' % commonsCatTemplateName)
-        if commonsCatTemplate in page.templates():
+        if is_template_present_in_page(commonsCatTemplate, page):
             result.add(getCategoryFromCommonscat(page, commonsCatTemplates))
     if not len(result):
         try:
@@ -409,7 +414,7 @@ def getCategories(page, commonsCatTemplates):
             for commonsCatTemplateName in commonsCatTemplates:
                 commonsCatTemplate = pywikibot.Page(page.site, 'Template:%s' % commonsCatTemplateName)
                 # print commonsCatTemplate
-                if commonsCatTemplate in cat.templates():
+                if is_template_present_in_page(commonsCatTemplate, cat):
                     # print u'hit!'
                     result.add(
                         getCategoryFromCommonscat(cat, commonsCatTemplates))

commit 13feb27ebfdd2e764e35bd063cada9f2301ed2e7

Fix bug introduced in 50e3ce9d
Change-Id: I69ac0a1801830f5ff6b4a57f9bad33a564c110a9

erfgoedbot/categorize_images.py
@@ -283,7 +283,7 @@ def get_new_categories(monumentId, monData, lang, commonsCatTemplates):
                 try:
                     for commonsCatTemplateName in commonsCatTemplates:
                         commonsCatTemplate = pywikibot.Page(project_site, 'Template:%s' % commonsCatTemplateName)
-                        if is_template_present_in_page(commonsCatTemplate, monumentArticle.templates):
+                        if is_template_present_in_page(commonsCatTemplate, monumentArticle):
                             newcats = []
                             newcats.append(
                                 getCategoryFromCommonscat(monumentArticle, commonsCatTemplates))