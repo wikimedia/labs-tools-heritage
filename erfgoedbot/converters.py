7a09e235534c963154a48e614810d9662167b914
Jean-Fred <jeanfrederic.wiki@gmail.com>
2015-09-18 14:01:28 +0100

    Move conversion methods to own module
    So it can be independently testable.
    
    Change-Id: I2d852f26ba03ffa41cc4ab1b18dced8d56f85e2f

---
+++ erfgoedbot/converters.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+
+"""Conversion methods"""
+
+import re
+
+
+def CH1903Converter(x, y):
+    if not(x.strip() and y.strip()):
+        # x or y is empty
+        return (0, 0)
+    x = float(x)
+    y = float(y)
+
+    lat = 16.9023892
+    lat = lat + 3.238272 * (y - 200) / 1000
+    lat = lat + 0.270978 * (x - 600) / 1000 * (x - 600) / 1000
+    lat = lat + 0.002528 * (y - 200) / 1000 * (y - 200) / 1000
+    lat = lat + 0.044700 * \
+        (x - 600) / 1000 * (x - 600) / 1000 * (y - 200) / 1000
+    lat = lat + 0.014000 * \
+        (y - 200) / 1000 * (y - 200) / 1000 * (y - 200) / 1000
+    lat = lat / 0.36  # Round 6
+
+    lon = 2.6779094
+    lon = lon + 4.728982 * (x - 600) / 1000
+    lon = lon + 0.791484 * (x - 600) / 1000 * (y - 200) / 1000
+    lon = lon + 0.130600 * \
+        (x - 600) / 1000 * (y - 200) / 1000 * (y - 200) / 1000
+    lon = lon - 0.043600 * \
+        (x - 600) / 1000 * (x - 600) / 1000 * (x - 600) / 1000
+    lon = lon / 0.36  # Round 6
+
+    return (lat, lon)
+
+
+def ucfirst(text):
+    if (text):
+        return text[0].upper() + text[1:]
+    else:
+        return ''
+
+
+def extractWikilink(text):
+    articleName = u''
+    result = re.search("\[\[(.+?)(\||\]\])", text)
+    if (result and result.group(1)):
+        articleName = result.group(1)
+        articleName = articleName.replace(u' ', u'_')
+        articleName = ucfirst(articleName)
+
+    return articleName
--- erfgoedbot/update_database.py
+++ erfgoedbot/update_database.py
@@ -17,8 +17,8 @@ import monuments_config as mconfig
 import pywikibot
 import MySQLdb
 from pywikibot import config
-import re
 from pywikibot import pagegenerators
+from converters import CH1903Converter, extractWikilink
 
 
 def connectDatabase():
@@ -29,53 +29,6 @@ def connectDatabase():
                            passwd=config.db_password, use_unicode=True, charset='utf8')
     cursor = conn.cursor()
     return (conn, cursor)
-
-
-def CH1903Converter(x, y):
-    if not(x.strip() and y.strip()):
-        # x or y is empty
-        return (0, 0)
-    x = float(x)
-    y = float(y)
-
-    lat = 16.9023892
-    lat = lat + 3.238272 * (y - 200) / 1000
-    lat = lat + 0.270978 * (x - 600) / 1000 * (x - 600) / 1000
-    lat = lat + 0.002528 * (y - 200) / 1000 * (y - 200) / 1000
-    lat = lat + 0.044700 * \
-        (x - 600) / 1000 * (x - 600) / 1000 * (y - 200) / 1000
-    lat = lat + 0.014000 * \
-        (y - 200) / 1000 * (y - 200) / 1000 * (y - 200) / 1000
-    lat = lat / 0.36  # Round 6
-
-    lon = 2.6779094
-    lon = lon + 4.728982 * (x - 600) / 1000
-    lon = lon + 0.791484 * (x - 600) / 1000 * (y - 200) / 1000
-    lon = lon + 0.130600 * \
-        (x - 600) / 1000 * (y - 200) / 1000 * (y - 200) / 1000
-    lon = lon - 0.043600 * \
-        (x - 600) / 1000 * (x - 600) / 1000 * (x - 600) / 1000
-    lon = lon / 0.36  # Round 6
-
-    return (lat, lon)
-
-
-def ucfirst(text):
-    if (text):
-        return text[0].upper() + text[1:]
-    else:
-        return ''
-
-
-def extractWikilink(text):
-    articleName = u''
-    result = re.search("\[\[(.+?)(\||\]\])", text)
-    if (result and result.group(1)):
-        articleName = result.group(1)
-        articleName = articleName.replace(u' ', u'_')
-        articleName = ucfirst(articleName)
-
-    return articleName
 
 
 def reportDataError(errorMsg, wikiPage, exceptWord, comment=''):
