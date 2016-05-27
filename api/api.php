688141a7b13e244a9dd76faf454af2f934cc3e41
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2016-05-27 08:54:13 +0100

    Move/Merge initialization bits from api.php to common.php
    Common.php already has most of the necessary setup bits,
    which is duplicated in api.php
    
    Also do not import Intuition if not available.
    
    Change-Id: I86ecdba64d3702a990f42b11f9c42bf419831539

--- api/api.php
+++ api/api.php
@@ -1,36 +1,6 @@
 <?php
 
 /* Entry point for the monuments api */
-require dirname( __FILE__ ) . '/autoloader.php';
-require dirname( __FILE__ ) . '/includes/Defaults.php';
-/**
- * Look for config overrides
- *
- * First look in p_erfgoed's parent dir, then in p_erfgoed root.
- * Otherwise, erfgoed will just use Defaults.php.
- */
-$config_override = 'database.inc';
-if ( file_exists( dirname( dirname( dirname( __FILE__ ) ) ) . "/{$config_override}" ) ) {
-	require dirname( dirname( dirname( __FILE__ ) ) ) . "/{$config_override}";
-} elseif ( file_exists( dirname( dirname( __FILE__ ) ) . "/{$config_override}" ) ) {
-	require dirname( dirname( __FILE__ ) ) . "/{$config_override}";
-}
-
-/* Localization */
-if ( $tsI18nDir ) {
-	require_once( "$tsI18nDir/ToolStart.php" );
-	$opts = array(
-		'domain' => 'MonumentsAPI', // name of your main text-domain here
-		'globalfunctions' => true, // defines _(), _e() and _g() as shortcut for $I18N->msg( .. )
-		'suppressnotice' => true,
-	);
-	$I18N = new TsIntuition( $opts );
-}
-
-$dbStatus = Database::define($dbServer, $dbDatabase, $dbUser,
-	$dbPassword );
-if (!$dbStatus) {
-	die( "Coudn't connect to db! ". mysql_error() );
-}
+require dirname( __FILE__ ) . '/common.php';
 
 ApiMain::dispatch();
--- api/common.php
+++ api/common.php
@@ -1,28 +1,43 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', true);
-ini_set('html_errors', false);
+ini_set('html_errors', true);
 
 /* Entry point for the monuments api */
 require dirname( __FILE__ ) . '/autoloader.php';
 require dirname( __FILE__ ) . '/includes/Defaults.php';
-require dirname( dirname( dirname( __FILE__ ) ) ) . '/database.inc';
+
+/**
+ * Look for config overrides
+ *
+ * First look in p_erfgoed's parent dir, then in p_erfgoed root.
+ * Otherwise, erfgoed will just use Defaults.php.
+ */
+$config_override = 'database.inc';
+if ( file_exists( dirname( dirname( dirname( __FILE__ ) ) ) . "/{$config_override}" ) ) {
+	require dirname( dirname( dirname( __FILE__ ) ) ) . "/{$config_override}";
+} elseif ( file_exists( dirname( dirname( __FILE__ ) ) . "/{$config_override}" ) ) {
+	require dirname( dirname( __FILE__ ) ) . "/{$config_override}";
+}
 
 /* Localization */
-require_once( "$tsI18nDir/ToolStart.php" );
+if ( $tsI18nDir ) {
+	require_once( "$tsI18nDir/ToolStart.php" );
+	$opts = array(
+		'domain' => 'MonumentsAPI', // name of your main text-domain here
+		'globalfunctions' => true, // defines _(), _e() and _g() as shortcut for $I18N->msg( .. )
+		'suppresserrors' => false, // Krinkle heeft het stukgemaakt
+		);
+	$I18N = new TsIntuition( $opts );
+} else {
+	$I18N = null;
+}
 
 $dbStatus = Database::define($dbServer, $dbDatabase, $dbUser,
 	isset( $toolserver_password )? $toolserver_password : $dbPassword );
 if (!$dbStatus) {
 	die( "Coudn't connect to db! ". mysql_error() );
 }
-
-$opts = array(
-	'domain' => 'MonumentsAPI', // name of your main text-domain here
-	'globalfunctions' => true, // defines _(), _e() and _g() as shortcut for $I18N->msg( .. )
-	'suppresserrors' => false, // Krinkle heeft het stukgemaakt
-	);
-$I18N = new TsIntuition( $opts );
 
 header ('Content-type: text/html; charset=utf-8');
 
