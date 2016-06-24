4bdddd08b0e328ab3bd63be206e919b5e7785973
lokal-profil <lokal.profil@gmail.com>
2016-06-25 01:26:54 +0200

    Recreate source table prior to run
    Triggers a recreation of the source tabls prior to each run in order
    to ensure that the latest settings in monuments_config.py are used.
    
    Also:
    * fix some indentation in the related create_all_monuments_table
      script.
    * Improve wording of shell script outputs
    * Add "Done" shell script output
    * Add timestamps to shell script outputs
    * Fix shell issues per http://www.shellcheck.net/
    
    Bug: T138606
    Change-Id: I1056de9a997b86a9894506261400a570c8ae28de

--- bin/create_all_monuments_tables.sh
+++ bin/create_all_monuments_tables.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Script to create all the tables from scratch
 # The python script expects to be in it's own directory:
-cd /data/project/heritage/erfgoedbot
+cd /data/project/heritage/erfgoedbot || exit
 # First create the sql statements
 python /data/project/heritage/erfgoedbot/monument_tables.py
-for i in `ls /data/project/heritage/erfgoedbot/sql/create_table_monuments*`
+for i in /data/project/heritage/erfgoedbot/sql/create_table_monuments*
 do
-	echo $i
-         mysql -h tools-db s51138__heritage_p < $i
+    echo "$i"
+    mysql -h tools-db s51138__heritage_p < "$i"
 done
-for i in `ls /data/project/heritage/erfgoedbot/sql/create_table_wlpa_*`
+for i in /data/project/heritage/erfgoedbot/sql/create_table_wlpa_*
 do
-         mysql -h tools-db s51138__heritage_p < $i
+    echo "$i"
+    mysql -h tools-db s51138__heritage_p < "$i"
 done
 # Admin tree for browsing ISO codes
 mysql -h tools-db s51138__heritage_p < /data/project/heritage/erfgoedbot/sql/create_table_admin_tree.sql
