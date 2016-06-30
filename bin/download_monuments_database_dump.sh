0154a319eb773eb33c6f5b549f32b1ebb5207e2f
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2016-06-30 17:32:47 +0100

    Fix ShellCheck errors in bin scripts
    - Add shebang for download_monuments_database_dump.sh
    - Fix SC2164: Use cd ... || exit in case cd fails.
    - Fix SC2035: Use ./*.txt so names with dashes won't become options
    
    Change-Id: I2a3819a4623d2ad29b4e356a795d595b1dcc9055

--- bin/download_monuments_database_dump.sh
+++ bin/download_monuments_database_dump.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# Downloads the monuments database dump from Tool Labs
+
 wget https://tools.wmflabs.org/heritage/monuments_db.sql.gz ./conf/
--- bin/dump_for_wmf.sh
+++ bin/dump_for_wmf.sh
@@ -2,10 +2,10 @@
 
 # Dump for wlm.wikipedia.org
 #mysqldump --skip-add-locks --complete-insert --default-character-set=utf8 p_erfgoed_p monuments_all admin_tree|gzip>./public_html/monuments.sql.gz
-cd ~/temp
+cd ~/temp || exit
 php ../tools/export_as_text.php admin_tree > ./admin_tree_tmp.txt
 php ../tools/export_as_text.php monuments_all > ./monuments_all_tmp.txt
 tar -czf ../public_html/export.tar.gz admin_tree_tmp.txt monuments_all_tmp.txt
-rm *.txt
+rm ./*.txt
 cd ..
 rm /home/project/e/r/f/erfgoed/temp/countries.ser
