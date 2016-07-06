1022e80c26d58e438dcfb890a5636be2a3df36f3
lokal-profil <lokal.profil@gmail.com>
2016-07-06 17:45:41 +0200

    Remove old sql dump before downloading new
    For me at least wget does not overwrite but downloads the new one
    as [...].gz.1
    
    Change-Id: I66de8f088026ec4f85476cfb8560935c379cead9

--- bin/download_monuments_database_dump.sh
+++ bin/download_monuments_database_dump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Downloads the monuments database dump from Tool Labs
 
-wget https://tools.wmflabs.org/heritage/monuments_db.sql.gz -P ./conf/
+wget --timestamping https://tools.wmflabs.org/heritage/monuments_db.sql.gz -P ./conf/
