b614e78be5b0b97d2bf19f6be1c83219ae7d53ee
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2016-07-19 23:30:02 +0100

    Add two known fields to fr (fr)
    Change-Id: I178493962e4f29fc79a18c80dd9c8858482e99c8

--- erfgoedbot/monuments_config.py
+++ erfgoedbot/monuments_config.py
@@ -3663,6 +3663,14 @@ countries = {
                 'dest': u'tri adresse',
             },
             {
+                'source': u'département',
+                'dest': u'département',
+            },
+            {
+                'source': u'tri département',
+                'dest': u'tri_departement',
+            },
+            {
                 'source': u'latitude',
                 'dest': u'lat',
                 'check': u'checkLat',
