8d7de41f0abfea2c16d955421dfbecc44e0f5c5d
lokal-profil <lokal.profil@gmail.com>
2016-08-08 10:37:45 +0200

    Add commonscat mapping for sq.wikipedia
    Bug: T141505
    Change-Id: Id59a59c1456832e8bd09a239a951a74231158116

--- erfgoedbot/categorize_images.py
+++ erfgoedbot/categorize_images.py
@@ -108,6 +108,7 @@ wikipedia_commonscat_templates = {
     'sl': (u'Kategorija v Zbirki',
            [u'Commonscat', u'Kategorija v zbirki', u'Commons cat',
             u'Katzbirke']),
+    'sq': (u'Commonscat', [u'Commonskat', u'Commonsart', u'CommonsCat']),
     'sv': (u'Commonscat',
            [u'Commonscat-rad', u'Commonskat', u'Commons cat', u'Commonscatbox',
             u'Commonscat-box']),
