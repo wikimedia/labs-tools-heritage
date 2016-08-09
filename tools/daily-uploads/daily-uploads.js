1603efb1af7e0647e882b984a13b43fe6d6f1838
Édouard Hue <edouard.hue@gmail.com>
2016-08-10 00:17:02 +0200

    Importing daily-uploads
    * Put the code under /tools/
    * Added an item to main heritage menu
    * Added an entry to toolinfo
    * Reworked volume mounting in docker-compose to mimic toollabs layout
    
    Bug: T142570
    Change-Id: Ib466626a7529c57abdffedf2bf8ac34cf2da87c4

---
+++ tools/daily-uploads/daily-uploads.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Édouard Hue on 09/08/16.
+ */
+
+var $grid = $("#images").isotope({
+    initLayout: false,
+    itemSelector: '.grid-item',
+    layoutMode: 'masonry',
+    masonry: {
+        columnWidth: '.grid-sizer'
+    }
+});
+
+$grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+});
+
+var dailyUploads = function() {
+
+    function viewModel() {
+        var self = this;
+
+        self.day = ko.observable("2016-05-02");
+        self.category = ko.observable("Images from Wiki Loves Earth 2016 in France");
+        self.images = ko.observableArray();
+
+
+        self.updateImages = function () {
+            self.images.removeAll();
+            var start = new Date(self.day());
+            start.setHours(0);
+            start.setMinutes(0);
+            start.setSeconds(0);
+            start.setMilliseconds(0);
+            var end = new Date(self.day());
+            end.setHours(23);
+            end.setMinutes(59);
+            end.setSeconds(59);
+            end.setMilliseconds(999);
+            self.continueUpdate({
+                start: start,
+                end: end
+            });
+        };
+
+        self.continueUpdate = function (boundaries, queryContinue) {
+            var data = {
+                action: "query",
+                prop: "imageinfo",
+                iiprop: "url",
+                iiurlwidth: 320,
+                generator: "categorymembers",
+                gcmtitle: "Category:" + self.category(),
+                gcmtype: "file",
+                gcmprop: "title",
+                gcmlimit: 50,
+                gcmsort: "timestamp",
+                gcmstart: boundaries.start.toUTCString(),
+                gcmend: boundaries.end.toUTCString(),
+                format: "json"
+            };
+
+            if (queryContinue) {
+                for (var attrname in queryContinue) {
+                    data[attrname] = queryContinue[attrname];
+                }
+            } else {
+                data.continue = "";
+            }
+
+            $.getJSON("//commons.wikimedia.org/w/api.php?callback=?", data, function (data) {
+                $.each(data.query.pages, function (index, file) {
+                    var image = {
+                        title: file.title,
+                        thumburl: file.imageinfo[0].thumburl,
+                        descurl: file.imageinfo[0].descriptionurl
+                    };
+                    self.images.push(image);
+                });
+                if ("continue" in data) {
+                    self.continueUpdate(boundaries, data.continue);
+                }
+            });
+        };
+    };
+
+    ko.applyBindings(viewModel);
+}
