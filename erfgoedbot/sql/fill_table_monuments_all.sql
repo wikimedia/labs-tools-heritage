82147b8dde1542d9706ebc98ca1df28e0b660385
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2016-10-24 19:29:43 +0100

    Harvest Wikidata item for ad (ca)
    Bug: T140795
    
    Change-Id: Ifdad87de2a4789bffe8c55c33cc29498617096a2

--- erfgoedbot/sql/fill_table_monuments_all.sql
+++ erfgoedbot/sql/fill_table_monuments_all.sql
@@ -52,7 +52,7 @@ TRUNCATE TABLE `monuments_all_tmp`;
 /* Andorra in Catalan */
 REPLACE INTO
   `monuments_all_tmp` (
-    `country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`, `lon`, `lat_int`, `lon_int`, `image`, `commonscat`, `source`, `changed`, `monument_article`, `registrant_url`
+    `country`, `lang`, `id`, `adm0`, `adm1`, `adm2`, `adm3`, `adm4`, `name`, `address`, `municipality`, `lat`, `lon`, `lat_int`, `lon_int`, `image`, `commonscat`, `source`, `changed`, `monument_article`, `registrant_url`, `wd_item`
   ) SELECT
     'ad' AS `country`,
     'ca' AS `lang`,
@@ -74,7 +74,8 @@ REPLACE INTO
     `source` AS `source`,
     `changed` AS `changed`,
     `monument_article` AS `monument_article`,
-    `registrant_url` AS `registrant_url`
+    `registrant_url` AS `registrant_url`,
+    `wd_item` AS `wd_item`
     FROM `monuments_ad_(ca)`;
 
 /* Albania in Albanian */
