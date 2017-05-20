196c165d5e9c5dfe3fe8bd6ad13947a9e8186fac
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2017-05-20 14:45:00 +0100

    Extract paths to constant in execution bin script
    The full paths to the Pywikibot binary and to the erfgoed library
    were hardcoded in many places in the harvesting bin scripts.
    
    This extracts these full paths into constants at the beginning
    of the scripts, which make it easier to read and change.
    
    Change-Id: Iede20ddf1cbbe4d327f7afe42c8f8694b4c0d8b4

--- bin/update_monuments_min.sh
+++ bin/update_monuments_min.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
 # Minimal script to update the monuments database
+PYWIKIBOT_BIN=/data/project/heritage/pywikibot/pwb.py
+ERFGOED_PATH=/data/project/heritage/erfgoedbot
 
 # First have the erfgoed bot update everything in it's config
-/data/project/heritage/pywikibot/pwb.py /data/project/heritage/erfgoedbot/update_database.py -fullupdate
+$PYWIKIBOT_BIN $ERFGOED_PATH/update_database.py -fullupdate
 
 # Update the all monuments table
-mysql -h tools-db s51138__heritage_p < /data/project/heritage/erfgoedbot/sql/fill_table_monuments_all.sql
+mysql -h tools-db s51138__heritage_p < $ERFGOED_PATH/sql/fill_table_monuments_all.sql
