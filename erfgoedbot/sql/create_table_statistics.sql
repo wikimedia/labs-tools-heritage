783aca69e1f7abbee591669784d60045c51b9d24
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2017-09-19 19:20:26 +0100

    Add SQL table for `statistics`
    This adds a `statistics` and `statisticsct` table based
    on `maintenance/INSTALL` and `api/includes/StatsBuilder`.
    
    This is necessary to develop locally on the Statistics module.
    
    Change-Id: Iaa81caab01a236e67fe9fe5ec8587e3a1efa6041

---
+++ erfgoedbot/sql/create_table_statistics.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS `statistics` (
+  `day` date NOT NULL,
+  `item` varchar(50) NOT NULL,
+  `idx` varchar(100) NOT NULL,
+  `value` varchar(16) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`day`,`item`,`idx`),
+  KEY `idx_day_idx` (`day`,`idx`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `statisticsct` (
+  `img_name` varbinary(255) NOT NULL,
+  `img_user_id` int(5) NOT NULL,
+  `img_user_name` varchar(255) NOT NULL,
+  `img_page_id` int(8) NOT NULL,
+  `img_wlm_country` varchar(20) NOT NULL DEFAULT '',
+  `img_wlm_id` varchar(25) NOT NULL DEFAULT '',
+  `img_timestamp` varchar(14) NOT NULL,
+  `img_has_changed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`img_name`),
+  KEY `idx_wlm_id` (`img_wlm_id`),
+  KEY `idx_ts` (`img_timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
