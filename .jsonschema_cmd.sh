6caa9beb4a1654650fb992d16a876d650361f096
Antoine Musso <hashar@free.fr>
2023-09-12 11:47:49 +0200

    Validate the monuments_config.schema is a schema
    `check-jsonschema` can validate a schema against the metaschema it
    refered to by "$schema" by using `--check-metaschema`. It ensures the
    schema is indeed a valid schema.
    
    Example output for an invalid schema:
    
      $ tox -e jsonschema
      jsonschema: commands[0]> .jsonschema_cmd.sh
      Validating schema ./erfgoedbot/monuments_config/monuments_config.schema
      Schema validation errors were encountered.
        erfgoedbot/monuments_config/monuments_config.schema::$.properties.lang.type:
        'strazeing' is not valid under any of the given schemas
        Underlying errors caused this.
        Best Match:
          $.properties.lang.type: 'strazeing' is not one of ['array',
          'boolean', 'integer', 'null', 'number', 'object', 'string']
      ERROR: Invalid schema file: ./erfgoedbot/monuments_config/monuments_config.schema
      jsonschema: exit 1 (0.20 seconds) ./labs/tools/heritage> .jsonschema_cmd.sh pid=1509573
    
    For a valid schema:
    
      $ tox -e jsonschema
      jsonschema: commands[0]> .jsonschema_cmd.sh
      Validating schema ./erfgoedbot/monuments_config/monuments_config.schema
      ok -- validation done
    
      Processing json configs in ./erfgoedbot/monuments_config
      ok -- validation done
      √ ./erfgoedbot/monuments_config/ch_it.json
      ok -- validation done
      √ ./erfgoedbot/monuments_config/ve_es.json
      ok -- validation done
    
    Change-Id: I9289f6d4f588e0c67755d8604825bbc898aea8a7

--- .jsonschema_cmd.sh
+++ .jsonschema_cmd.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 JSON_PATH=./erfgoedbot/monuments_config
 
+SCHEMA_FILE=$JSON_PATH/monuments_config.schema
+
+echo "Validating schema $SCHEMA_FILE"
+if ! check-jsonschema --check-metaschema $SCHEMA_FILE; then
+    >&2 echo "ERROR: Invalid schema file: $SCHEMA_FILE"
+    exit 1
+fi
+
+echo
+echo "Processing json configs in $JSON_PATH"
 files=$(find $JSON_PATH -name '*.json' -print)
 
 FAIL=0
 
 for f in $files; do
     if [ -e "$f" ]; then
-        (check-jsonschema --schemafile="$JSON_PATH/monuments_config.schema" "$f" && echo "√ $f") || FAIL=$((FAIL+1))
+        (check-jsonschema --schemafile=$SCHEMA_FILE "$f" && echo "√ $f") || FAIL=$((FAIL+1))
     fi
 done
 if [[ $FAIL -ne 0 ]];  then
